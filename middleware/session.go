bf06188f5dfeec8f6cd90e1721ea1423aa83ff40
usysrc <usysrc@usysrc.dev>
2024-12-26 23:19:59 +0100

    Feat: integrate SQLite storage for session management

--- middleware/session.go
+++ middleware/session.go
@@ -3,12 +3,21 @@ package middleware
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
+	"github.com/gofiber/storage/sqlite3"
 )
 
 var sessionStore *session.Store
 
 func CreateSessionStore() {
-	sessionStore = session.New()
+	// Create a SQLite storage instance
+	storage := sqlite3.New(sqlite3.Config{
+		Database: "./fiber_session.db", // Path to your SQLite database file
+		Table:    "sessions",           // Table name for storing session data
+	})
+
+	sessionStore = session.New(session.Config{
+		Storage: storage, // Use the SQLite storage
+	})
 }
 
 // Middleware to initialize session
